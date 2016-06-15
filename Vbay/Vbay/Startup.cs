71f81b02fde2609141685efdec253070351e2b60
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-15 09:47:00 -0400

    Initial

---
+++ Vbay/Vbay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vbay.Startup))]
+namespace Vbay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
