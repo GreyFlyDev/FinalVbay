30646bf148f98aa7b34306e6170f5f0da101f41d
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-15 09:50:00 -0400

    Added Ad Entity

---
+++ Vbay/Vbay/Models/Ad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vbay.Models
+{
+    public class Ad
+    {
+        public int Id { get; set; }
+        public string Headline { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool Approved { get; set; }
+        public DateTime DatePosted { get; set; }
+        
+        public string UserId { get; set; }
+    }
+}
