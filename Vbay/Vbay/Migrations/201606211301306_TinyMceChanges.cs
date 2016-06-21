34a37f8793c6006e3d7ccdc68c961c1d777e5904
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-21 09:15:39 -0400

    Fixed Wysiwyg Html tag problem. Still problem with EditorFor(model.Description)

---
+++ Vbay/Vbay/Migrations/201606211301306_TinyMceChanges.cs
@@ -0,0 +1,16 @@
+namespace Vbay.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TinyMceChanges : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Vbay/Vbay/Models/Ad.cs
+++ Vbay/Vbay/Models/Ad.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,7 @@ namespace Vbay.Models
         public int Id { get; set; }
         public string Headline { get; set; }
         [AllowHtml]
+        [UIHint("tinymce_full_compressed")]
         public string Description { get; set; }
         public decimal Price { get; set; }
         public bool Approved { get; set; }
