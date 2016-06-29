d7eb2d346c9fcdbb25acac0486f3dd39bf41c8c6
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-29 13:22:28 -0400

    Added truncated description and headlines to list

--- Vbay/Vbay/Models/Ad.cs
+++ Vbay/Vbay/Models/Ad.cs
@@ -20,6 +20,30 @@ namespace Vbay.Models
         public DateTime DatePosted { get; set; }
         public string AdminComments { get; set; }
         public string AdminFilter { get; set; }
+
+        private int shortDescriptionLength = 35;
+        private int shortHeadlineLength = 15;
+
+        public string ShortHeadline
+        {
+            get
+            {
+                if (Headline.Length > shortHeadlineLength)
+                    return Headline.Substring(0, shortHeadlineLength) + "...";
+                else
+                    return Headline;
+            }
+        }
+        public string ShortDescription
+        {
+            get
+            {
+                if (Description.Length > shortDescriptionLength)
+                    return Description.Substring(0, shortDescriptionLength) + "...";
+                else
+                    return Description;
+            }
+        }
         
         public string UserId { get; set; }
     }
